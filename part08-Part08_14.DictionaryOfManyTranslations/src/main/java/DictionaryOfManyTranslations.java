28557b89701c56761fab3ae5f0de943fa21ec095
Aliyasar-celik <aliyasarcelik33@hotmail.com>
2026-01-20 16:58:34 +0300

    done part 14

--- part08-Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
+++ part08-Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
@@ -1,22 +1,39 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class DictionaryOfManyTranslations {
 
+    // the Exercise dictates English to Finnish
     private HashMap<String, ArrayList<String>> translationsHashMap;
 
     public DictionaryOfManyTranslations() {
-        this.translationsHashMap = new HashMap();
+        this.translationsHashMap = new HashMap<String, ArrayList<String>>();
     }
 
     public void add(String word, String translation) {
 
         this.translationsHashMap.putIfAbsent(word, new ArrayList<String>());
 
+        ArrayList<String> translations = this.translationsHashMap.get(word);
+
+        translations.add(translation);
+
     }
 
     public ArrayList<String> translate(String word) {
 
+        if (this.translationsHashMap.containsKey(word)) {
+            return this.translationsHashMap.get(word);
+        }
+
+        return new ArrayList<String>();
+
+    }
+
+    public void remove(String word) {
+
+        this.translationsHashMap.remove(word);
 
     }
 
