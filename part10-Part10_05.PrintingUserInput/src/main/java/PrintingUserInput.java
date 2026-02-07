39a152e379a7b347a2ccc4eb1d09d84611c7ebe7
Aliyasar-celik <aliyasarcelik33@hotmail.com>
2026-02-07 20:51:23 +0300

    Done some exercises to practice some stream functionalities

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -5,7 +5,19 @@ import java.util.Scanner;
 public class PrintingUserInput {
 
     public static void main(String[] args) {
+        ArrayList<String> inputs = new ArrayList<String>();
         Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
+
+        while (!input.isEmpty()) {
+
+            inputs.add(input);
+            input = scanner.nextLine();
+
+        }
+
+        scanner.close();
+        inputs.stream().forEach(name -> System.out.println(name));
     }
 }
--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -5,7 +5,23 @@ import java.util.Scanner;
 public class LimitedNumbers {
 
     public static void main(String[] args) {
+
+        ArrayList<Integer> inputs = new ArrayList<Integer>();
         Scanner scanner = new Scanner(System.in);
+
+        int input = Integer.valueOf(scanner.nextLine());
+
+        while (input > 0) {
+
+            inputs.add(input);
+            input = Integer.valueOf(scanner.nextLine());
+
+        }
+
+        scanner.close();
+        inputs.stream()
+                .filter(i -> (i >= 1) && (i <= 5 ))
+                .forEach(i -> System.out.println(i));
 
     }
 }
